58fe7c81ec6e9c9f55300e082bacfc19d5a7988b
Mario Rovito <alessandro.r14@gmail.com>
2022-03-14 15:07:07 +0100

    add prompt

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ console.log('JS OK!')
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-
+// Chiedere all’utente di inserire una parola
+var parola = prompt('Inserisci una parola ');
 
 
 
